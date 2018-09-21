7b5e4b106ef2f772b9b807dbd0f92cba6ed41043
phpf <phpfrmwrk@gmail.com>
2018-09-21 17:54:50 +0200

    First!

---
+++ src/DataType.php
@@ -0,0 +1,19 @@
+<?php
+namespace phpf\typ;
+abstract class DataType {
+	
+	protected static $shortType = 's';
+	
+	public function __construct(){
+		
+	}
+	
+	public function getValue(){
+		return $this->value;
+	}
+	
+	public static function getShortType(){
+		return static::$shortType;
+	}
+}
+?>
